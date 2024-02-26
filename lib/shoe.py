257287c1e5cbd1fa38b72ce003cbf4fe8374142a
Kirstyn Canull <kirstyncanull@Kirstyns-MacBook-Air.local>
2024-02-26 13:08:34 -0500

    submitting lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self._condition = None
+    @property
+    def size(self):
+        return self._size 
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        self._size = size
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+        return True
+    
+    @property
+    def condition(self):
+        return self._condition
+    @condition.setter
+    def condition(self, condition):
+        self._condition = condition
+        if self.cobble():
+            print(self._condition == "New")
